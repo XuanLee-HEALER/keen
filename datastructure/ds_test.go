301f4156ea28c5e39f744b082cc7a586894dbf37
lixuan <lixuan@aixcloud.com.cn>
2023-12-28 18:10:19 +0800

    线程安全的优先级队列

--- datastructure/ds_test.go
+++ datastructure/ds_test.go
@@ -1,6 +1,7 @@
 package datastructure_test
 
 import (
+	"sync"
 	"testing"
 
 	"gitea.fcdm.top/lixuan/keen/datastructure"
@@ -21,12 +22,93 @@ func TestSetMerge(t *testing.T) {
 func TestQueue(t *testing.T) {
 	q := datastructure.NewQueue()
 
-	q.Push(1)
-	q.Push("something")
-	q.Push(2)
+	ori := []any{1, "x", 2, "y", 3, "z"}
+	des := ori
+
+	for _, v := range ori {
+		q.Enqueue(v)
+	}
+
+	counter := 0
+	for !q.Empty() {
+		// t.Log(counter)
+		if v := q.Dequeue(); v == nil || v != des[counter] {
+			t.FailNow()
+		}
+		counter++
+	}
+}
+
+func TestQueueDequeueEmpty(t *testing.T) {
+	q := datastructure.NewQueue()
+	if q.Dequeue() != nil {
+		t.FailNow()
+	}
+}
+
+func TestConcurrentQueueDequeueEmpty(t *testing.T) {
+	q := datastructure.NewConcurrentQueue()
+	if q.Dequeue() != nil {
+		t.FailNow()
+	}
+}
+
+func TestConcurrentQueue(t *testing.T) {
+	q := datastructure.NewConcurrentQueue()
+
+	ori := []any{1, "x", 2, "y", 3, "z"}
+
+	wg := new(sync.WaitGroup)
+	for i := range ori {
+		wg.Add(1)
+		go func(idx int) {
+			defer wg.Done()
+			q.Enqueue(ori[idx])
+		}(i)
+	}
+
+	wg.Wait()
 
 	for !q.Empty() {
-		t.Logf("value: %v", q.Remove())
+		t.Log(q.Dequeue())
+	}
+}
+
+func TestConcurrentPriorityQueueDequeueEmpty(t *testing.T) {
+	q := datastructure.NewConcurrentPriorityQueue(nil)
+	if q.Dequeue() != nil {
+		t.FailNow()
+	}
+}
+
+func TestConcurrentPriorityQueue(t *testing.T) {
+	q := datastructure.NewConcurrentPriorityQueue(func(a, b interface{}) int {
+		av := a.(int)
+		bv := b.(int)
+		if av < bv {
+			return -1
+		} else if av == bv {
+			return 0
+		} else {
+			return 1
+		}
+	})
+
+	ori := []any{1, 2, 1, 3, 4, 1, 2, 3, 4, 5, 2, 2}
+
+	wg := new(sync.WaitGroup)
+	for i := range ori {
+		wg.Add(1)
+		go func(idx int) {
+			defer wg.Done()
+			q.Enqueue(ori[idx])
+		}(i)
+	}
+
+	wg.Wait()
+
+	for !q.Empty() {
+		t.Log(q.Dequeue())
 	}
 }
 
--- datastructure/queue.go
+++ datastructure/queue.go
@@ -2,41 +2,82 @@ package datastructure
 
 import (
 	"container/list"
+	"sync"
+
+	pq "github.com/emirpasic/gods/queues/priorityqueue"
+	"github.com/emirpasic/gods/utils"
 )
 
 type Queue struct {
 	l *list.List
 }
 
-func NewQueue() Queue {
-	return Queue{
+func NewQueue() *Queue {
+	return &Queue{
 		l: new(list.List),
 	}
 }
 
-func (q Queue) Empty() bool {
-	return q.l.Front() == nil
+func (q *Queue) Enqueue(v any) {
+	q.l.PushBack(v)
 }
 
-func (q Queue) Push(e any) {
-	q.l.PushBack(e)
-}
-
-func (q Queue) Remove() any {
+func (q *Queue) Dequeue() any {
 	if q.l.Front() != nil {
-		r := q.l.Front()
-		v := r.Value
-		q.l.Remove(r)
-		return v
+		return q.l.Remove(q.l.Front())
 	}
 	return nil
 }
 
-func (q Queue) Peek() any {
+func (q *Queue) Peek() any {
 	if h := q.l.Front(); h != nil {
 		return h.Value
 	}
 	return nil
+}
+
+func (q *Queue) Empty() bool {
+	return q.l.Front() == nil
+}
+
+type ConcurrentQueue struct {
+	q *Queue
+
+	rwMut *sync.RWMutex
+}
+
+func NewConcurrentQueue() *ConcurrentQueue {
+	return &ConcurrentQueue{
+		NewQueue(),
+		&sync.RWMutex{},
+	}
+}
+
+func (q ConcurrentQueue) Enqueue(v any) {
+	q.rwMut.Lock()
+	defer q.rwMut.Unlock()
+	q.q.Enqueue(v)
+}
+
+func (q ConcurrentQueue) Dequeue() any {
+	q.rwMut.Lock()
+	defer q.rwMut.Unlock()
+
+	return q.q.Dequeue()
+}
+
+func (q ConcurrentQueue) Peek() any {
+	q.rwMut.RLock()
+	defer q.rwMut.RUnlock()
+
+	return q.q.Peek()
+}
+
+func (q ConcurrentQueue) Empty() bool {
+	q.rwMut.RLock()
+	defer q.rwMut.RUnlock()
+
+	return q.q.Empty()
 }
 
 type Stack struct {
@@ -72,4 +113,51 @@ func (s Stack) Peek() any {
 		return h.Value
 	}
 	return nil
+}
+
+type ConcurrentPriorityQueue struct {
+	q pq.Queue
+
+	rwMut *sync.RWMutex
+}
+
+func NewConcurrentPriorityQueue(cmp utils.Comparator) *ConcurrentPriorityQueue {
+	return &ConcurrentPriorityQueue{
+		*pq.NewWith(cmp),
+		&sync.RWMutex{},
+	}
+}
+
+func (q *ConcurrentPriorityQueue) Enqueue(v any) {
+	q.rwMut.Lock()
+	defer q.rwMut.Unlock()
+
+	q.q.Enqueue(v)
+}
+
+func (q *ConcurrentPriorityQueue) Dequeue() any {
+	q.rwMut.Lock()
+	defer q.rwMut.Unlock()
+
+	if v, ok := q.q.Dequeue(); ok {
+		return v
+	}
+	return nil
+}
+
+func (q *ConcurrentPriorityQueue) Peek() any {
+	q.rwMut.RLock()
+	defer q.rwMut.RUnlock()
+
+	if v, ok := q.q.Peek(); ok {
+		return v
+	}
+	return nil
+}
+
+func (q *ConcurrentPriorityQueue) Empty() bool {
+	q.rwMut.RLock()
+	defer q.rwMut.RUnlock()
+
+	return q.q.Empty()
 }
